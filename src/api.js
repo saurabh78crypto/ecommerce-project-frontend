4e17b776dad869d44057d38768a9af06793eb0ea
Saurabh Pingale <saurabhpingale93@gmail.com>
2024-08-03 19:27:45 +0530

    done

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = 'https://ecommerce-project-backend-gy68.onrender.com';
+const API_BASE_URL = 'https://ecommerce-project-backend-gy68.onrender.com/api/v1';
 
 const api = axios.create({
     baseURL: API_BASE_URL
